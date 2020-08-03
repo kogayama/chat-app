16392fd0220860b496b623e54da1945a53c6643c
kogayama <kogayama0130@gmail.com>
2020-08-04 08:18:00 +0900

    take create action by event

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,15 @@
 $(function(){
   $('.FormContents').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
