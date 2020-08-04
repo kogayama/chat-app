6fa33d3a10e6d3a6409c26bb5d518c0d05c65c89
kogayama <kogayama0130@gmail.com>
2020-08-04 09:39:26 +0900

    add done_method for HTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,43 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ){
+      let html = `<div class="Message">
+                    <div class="Message__info">
+                      <div class="Message__info__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="Message__info__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="Message__content">
+                      <p class="Message__content">
+                        ${message.content}
+                      </p>
+                      <img class="Message__image" src="${message.image}">
+                    </div>
+                  </div>`
+                return html;
+  } else {
+    let html = `<div class="Message">
+                  <div class="Message__info">
+                    <div class="Message__info__name">
+                      ${message.user_name}
+                    </div>
+                    <div class="Message__info__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="Message__content">
+                    <p class="Message__content">
+                      ${message.content}
+                    </p>
+                  </div>
+                </div>`
+                return html;
+          };
+  }
+
   $('.FormContents').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -11,5 +50,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-  })
-})
+    .done(function(data){
+      let html = buildHTML(data);
+    })
+  });
+});
